f4d6d75f98bbde78daa68aae1ecae65ad7a062ec
Web-Mastermind <azeagent@gmail.com>
2024-12-29 23:08:40 +0400

    hero page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import Link from "next/link"
+import styles from "./page.module.css"
+import MainSwiper from "../../components/swiper/MainSwiper"
 
 const page = () => {
   return (
-    <div className="container">
-      {/* <h1>Hello</h1> */}
-    </div>
+    <main className={styles.mainPage}>
+      <MainSwiper />
+      <div className="container">
+        <div className={styles.mainItems}>
+          <div className={styles.titleContainer}>
+            <p className={styles.mainText}>New Arrival</p>
+            <h1 className={styles.mainTitle}>Discover Our New Collection</h1>
+            <p className={styles.mainSecondText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+            <Link className={styles.mainButton} href="/">BUY NOW</Link>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
