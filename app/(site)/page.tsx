527cd1e3d9d1e549001b1f1a488b4492adc3edac
Web-Mastermind <azeagent@gmail.com>
2024-12-30 13:41:05 +0400

    added new section

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,22 +1,26 @@
 import Link from "next/link"
 import styles from "./page.module.css"
 import MainSwiper from "../../components/swiper/MainSwiper"
+import Browse from "@/components/browse/Browse"
 
 const page = () => {
   return (
-    <main className={styles.mainPage}>
-      <MainSwiper />
-      <div className="container">
-        <div className={styles.mainItems}>
-          <div className={styles.titleContainer}>
-            <p className={styles.mainText}>New Arrival</p>
-            <h1 className={styles.mainTitle}>Discover Our New Collection</h1>
-            <p className={styles.mainSecondText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-            <Link className={styles.mainButton} href="/">BUY NOW</Link>
+    <>
+      <main className={styles.mainPage}>
+        <MainSwiper />
+        <div className="container">
+          <div className={styles.mainItems}>
+            <div className={styles.titleContainer}>
+              <p className={styles.mainText}>New Arrival</p>
+              <h1 className={styles.mainTitle}>Discover Our New Collection</h1>
+              <p className={styles.mainSecondText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+              <Link className={styles.mainButton} href="/">BUY NOW</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+      <Browse />
+    </>
   )
 }
 
